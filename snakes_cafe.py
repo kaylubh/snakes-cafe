14856e16792de73a7930815d673ea9175223827a
Caleb Hemphill <calebhemp@gmail.com>
2024-01-08 23:02:08 -0500

    Add response when item is ordered

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,4 +1,4 @@
-print("""
+intro_menu = """
 **************************************
 **    Welcome to the Snakes Cafe!   **
 **    Please see our menu below.    **
@@ -34,23 +34,62 @@ Unicorn Tears
 ***********************************
 ** What would you like to order? **
 ***********************************
-""")
+"""
 
 order = {
-  appetizers: {
-
+  'appetizers': {
+    'Wings': 0,
+    'Cookies': 0,
+    'Spring Rolls': 0
   }, 
-  entrees: {
-    
+  'entrees': {
+    'Salmon': 0,
+    'Steak': 0,
+    'Meat Tornado': 0,
+    'A Literal Garden': 0
   },
-  desserts: {
-    
+  'desserts': {
+    'Ice Cream': 0,
+    'Cake': 0,
+    'Pie': 0
   },
-  drinks: {
-    
+  'drinks': {
+    'Coffee': 0,
+    'Tea': 0,
+    'Unicorn Tears': 0
   }
 }
 
+def handle_order_input(order_input):
+
+  if order_input == 'Quit':
+
+    return 'quit'
+  
+  else:
+
+    for course in order:
+      if order_input in order[course]:
+
+        order[course][order_input] += 1
+        quantity = order[course][order_input]
+
+        order_statement = f'{quantity} {"order" if quantity == 1 else "orders"} of {order_input} {"has" if quantity == 1 else "have"} been added to your meal'
+        response = f'\n ** {order_statement} ** \n'
+        
+        return response
+      
+  return '\n ** Not Found on the Menu; Check Spelling ** \n'
+
+print(intro_menu)
+
 order_input = ''
 while order_input.lower() != 'quit':
+  
   order_input = input('> ')
+  order_input = order_input.title()
+
+  response = handle_order_input(order_input)
+
+  if response != 'quit':
+    print(response)
